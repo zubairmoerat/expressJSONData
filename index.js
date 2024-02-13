c5c1a3548cba9e8c4d5ad9b20bbbe1ba0c3a13e6
zubairmoerat <zubairmoeratofficial@gmail.com>
2024-02-13 16:09:48 +0200

    not working

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import axios from 'axios'
+
 const app = express()
 const router = express.Router()
 const port = +process.env.PORT || 4000
@@ -6,23 +8,104 @@ const dataURL = 'https://zubairmoerat.github.io/zubairmoeratData/data/'
 app.use(
     router
 )
-// / => home
-router.get('^/$|/expressJSONData', (res, req)=>{
+router.get('^/$|/ejd',async(req, res)=>{
+    let response = await axios.get(dataURL)
+    let {home} = await response.data
     res.json({
         status: res.statusCode,
-        msg: "You're Home"
+        home
     })
 })
+// / => home
+// router.get('^/$|/expressJSONData', async(req, res)=>{
+//     let response = await fetch(dataURL)
+//     let {home} = await response.json()
+//     res.json({
+//         status: res.statusCode,
+//         home
+//     })
+// })
 // = education
-router.get('/education', async (res, req)=>{
-    let res = await fetch(dataURL).json()
-    let {education} = await res
+router.get('/education', async (req, res)=>{
+    let response = await fetch(dataURL)
+    let {education} = await response.json()
     res.json({
         status: res.statusCode,
         education
     })
 })
+// router.get('/education/:id', async(req, res)=>{
+//     let response = await fetch(dataURL)
+//     let {education} = await response.json()
+//     let params = +req.params.id
+//     let idx = params > 0 ? params - 1 : 0
+//     res.json({
+//         link: req.url,
+//         status: res.statusCode,
+//         education: education[idx]
+//     })
+// })
+router.post('/addEducation', async(req, res)=>{
+    try{
+        let response = await axios.post(dataURL,{
+            id: 5,
+            year: 2012,
+            description: "testing",
+            place: "Cape Town"
+        })
+        res.json({
+            method: req.method,
+            status: res.statusCode,
+            info: response.data
+        })
+        console.log(response.data);
+    }catch(e){
+        console.log('internal server error');
+    }
+})
+
+
+// router.patch('/updateEducation',async (req, res)=>{
+//     let response = await axios.patch(dataURL.education, {
+//         id: idx,
+//         year: int,
+//         description: "",
+//         place: ""
+//     })
+// })
+// router.delete('/deleteEducation',async (req, res)=>{
+//     let response = await axios.delete(dataURL, {
+//         id: idx,
+//         year: new Date().getFullYear(),
+//         description: "",
+//         place: ""
+//     })
+// })
+// router.get('/testimonials', async(req, res)=> {
+//     let response = await fetch(dataURL)
+//     let {testimonials} = await response.json()
+//     res.json({
+//         status: res.statusCode,
+//         testimonials
+//     })
+// })
+// router.get('/skills', async(req, res)=>{
+//     let response = await fetch(dataURL)
+//     let {skills} = await response.json()
+//     res.json({
+//         status: res.statusCode,
+//         skills
+//     })
+// })
+// router.get('/projects', async(req, res)=>{
+//     let response = await fetch(dataURL)
+//     let {projects} = await response.json()
+//     res.json({
+//         status: res.statusCode,
+//         projects
+//     })
+// })
 
 app.listen(port, ()=>{
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port - http://localhost:${port}`);
 })
